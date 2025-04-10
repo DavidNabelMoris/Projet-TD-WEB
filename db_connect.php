3426a8b632f42e719857da2141f053bfad7a098a
David <David.Nabel-Moris@etu.univ-smb.fr>
2025-04-10 10:43:02 +0200

    all

--- db_connect.php
+++ db_connect.php
@@ -1,6 +1,13 @@
 <?php
-$servername = "host.docker.internal"; 
+define('SERVEUR_BD',getenv('SERVEUR_BD'))
+define('LOGIN_BD',getenv('LOGIN_BD'))
+define('PASS_BD',getenv('PASS_BD'))
+define('NOM_BD',getenv('NOM_BD'))
+define('PASS_ROOT_BD',getenv('PASS_ROOT_BD'))
+define('PORT_WEB',getenv('PORT_WEB'))
+define('PORT_PHPMYADMIN ',getenv('PORT_PHPMYADMIN '))
 
+$servername = "host.docker.internal"; 
 $username = "userbd";
 $password = "1234";
 $dbname   = "mabase";
